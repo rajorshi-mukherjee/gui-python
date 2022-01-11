768b3e7ce799af766c9b56e2343d99907f38fe8e
Rajorshi Mukherjee <mr.rajorshi.mukherjee@gmail.com>
2022-01-11 22:17:57 +0530

    canvas
    Trying to create a canvas and testing out its features

---
+++ canvassing/coloured_canvas.py
@@ -0,0 +1,10 @@
+from tkinter import *   
+
+top = Tk()  
+top.geometry("200x400")  
+
+#creating a simple canvas  
+c = Canvas(top,bg = "pink",height = "200")  
+c = Canvas(top,bg = "pink",height = "400")  
+c.pack()  
+top.mainloop()  
